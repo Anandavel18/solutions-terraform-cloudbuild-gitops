08c2f39b8147bbc72fdf27f71e7073860a3e6e5b
anandavel <san.anandavel@gmail.com>
2021-07-25 12:15:40 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="famous-cursor-318312"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "famous-cursor-318312-tfstate"
     prefix = "env/prod"
   }
 }
